606d7b2fe892c0c2f6cce2b4c38a69a3947fc5e6
nate zhou <gnuunixchad@outlook.com>
2026-06-09 20:28:43 +0800

    feat: initial implementation

---
+++ README.md
@@ -0,0 +1,29 @@
+# stereoscopic
+
+Toggle a video filter that places two copies of the video side-by-side,
+creating a simple stereoscopic/dual-image illusion of 3D depth.
+
+![demo](./misc/demo.gif)
+
+# Dependency
+
+- mpv (compiled with FFmpeg feature enabled)
+
+The filter uses the `lavfi` filter which is typically available if mpv was
+compiled with FFmpeg support.
+
+# Installation
+
+```sh
+mkdir -p ${XDG_CONFIG_HOME}/mpv/scripts
+cp ./stereoscopic.lua ${XDG_CONFIG_HOME}/mpv/scripts/
+```
+
+# Usage
+
+Create a keybinding for this script to toggle the stereoscopic:
+
+```conf
+# $XDG_CONFIG_HOME/mpv/input.conf
+D script-message toggle-stereoscopic
+```
---
+++ stereoscopic.lua
@@ -0,0 +1,43 @@
+-- @author nate zhou
+-- @since 2026
+-- stereoscopic - duplicate the current playing video side-by-side to create the
+-- illusion of 3D depth.
+
+local mp = require 'mp'
+
+local enabled = false
+
+local function apply_filter()
+    local width = mp.get_property_native("width", 0)
+    local height = mp.get_property_native("height", 0)
+
+    if width > 0 and height > 0 then
+        local vf = string.format(
+            "lavfi=[split[a][b];[a]pad=%d:%d:0:0[base];[base][b]overlay=%d:0]",
+            width * 2, height, width
+        )
+        mp.set_property("vf", vf)
+    end
+    mp.osd_message("stereoscopic: on", 2)
+end
+
+local function remove_filter()
+    mp.set_property("vf", "")
+    mp.osd_message("stereoscopic: off", 2)
+end
+
+local function toggle_stereoscopic()
+    enabled = not enabled
+    if enabled then
+        apply_filter()
+    else
+        remove_filter()
+    end
+end
+
+mp.register_event("file-loaded", function()
+    enabled = false
+    remove_filter()
+end)
+
+mp.register_script_message("toggle-stereoscopic", toggle_stereoscopic)
